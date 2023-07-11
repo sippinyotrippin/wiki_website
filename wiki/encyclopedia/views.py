0b295e2af4a91038ffc4179f0108c1f1fc09ffaa
sippinyotrippin <van.didenko@gmail.com>
2023-07-11 13:21:39 +0300

    Entries are MarkDown-files and to render it on page it must be converted to HTML-files - implement convert_md_to_html function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,16 @@
+from markdown2 import Markdown
 from django.shortcuts import render
 
 from . import util
+
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content is None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 
 def index(request):
